9dfb94bd3e833bdef163312aa8d2c9787788fa55
John Galt <johngaltfirstrun@gmail.com>
2026-08-06 16:58:55 -0400

    Revert "iconloaderlib: fix dark/light theme inverted forced themed icons"
    This reverts commit 48b8b666d90f213788eab55075f36e1867439f0c.

--- iconloaderlib/src/com/android/launcher3/icons/mono/MonoThemedBitmap.kt
+++ iconloaderlib/src/com/android/launcher3/icons/mono/MonoThemedBitmap.kt
@@ -53,28 +53,10 @@ class MonoThemedBitmap(
             }
     }
 
-    fun getUpdatedColors(ctx: Context): IntArray {
-        val colors =
-            if (luminanceDelta != null) {
-                ColorAdapter(luminanceDelta).adaptedColorProvider(colorProvider)(ctx)
-            } else {
-                colorProvider(ctx)
-            }
-
-        // Forced themed icons are currently inverted relative to app-provided monochrome
-        // icons, so swap bg/fg unconditionally for that path.
-        return if (luminanceDelta != null) swapBgFg(colors) else colors
-    }
-
-    private fun swapBgFg(colors: IntArray): IntArray {
-        if (colors.size < 2)
-            return colors
-
-        return colors.clone().also {
-            it[0] = colors[1]
-            it[1] = colors[0]
-        }
-    }
+    fun getUpdatedColors(ctx: Context): ColorList =
+        if (luminanceDelta != null)
+            ColorAdapter(luminanceDelta).adaptedColorProvider(colorProvider)(ctx)
+        else colorProvider(ctx)
 
     companion object {
         const val DOUBLE_BYTE_SIZE = 8
